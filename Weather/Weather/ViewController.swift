ad85007d3082f71b87627fb66804fd34f79cff65
Your Name <rintudhaya@gmail.com>
2017-09-17 17:43:58 -0400

    Getting URL data

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -12,14 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let url = URL(string: Constants.darksky)
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                print("Error")
+            }
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+}
 }
 
