63b6d3daab62ccb7fb75214b4aa5799af0b23a49
Your Name <rintudhaya@gmail.com>
2017-09-17 17:46:12 -0400

    created struct for the url from DarkSky

---
+++ Weather/Weather/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by Daya on 9/17/17.
+//  Copyright © 2017 Daya. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let darksky = "https://api.darksky.net/forecast/96201d4434d0ef451b4d761f6609cf12/37.8267,-122.4233"
+}
